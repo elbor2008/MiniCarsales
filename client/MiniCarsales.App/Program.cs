e886d26281544b3376f697044a5fead856100cb0
Nick Nie <sky_elbor@hotmail.com>
2020-11-01 21:07:29 +0800

    change port for production model

--- client/MiniCarsales.App/Program.cs
+++ client/MiniCarsales.App/Program.cs
@@ -20,7 +20,8 @@ namespace MiniCarsales.App
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:3000");
                 });
     }
 }
